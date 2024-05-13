4ab363716124a330909f6bf662bc697962da324f
dustinquintanilla <portillo0121@gmail.com>
2024-05-13 11:50:09 -0400

    replaced textfield with textarea

--- src/editUserCredentials.java
+++ src/editUserCredentials.java
@@ -5,8 +5,9 @@ import java.nio.file.Paths;
 
 public class editUserCredentials {
     private JPanel panel1;
-    private JTextField textField1;
+    private JTextField txtArea;
     private JButton saveChangesButton;
+    private JTextArea textArea1;
 
     public editUserCredentials() {
         JFrame frame = new JFrame("Edit User Credentials");
@@ -17,12 +18,12 @@ public class editUserCredentials {
         frame.setLocationRelativeTo(null);
 
         String filePath = "src/resources/users.txt";
-        textField1.setText(loadFile(filePath));
-        textField1.setEditable(true);
+        txtArea.setText(loadFile(filePath));
+        txtArea.setEditable(true);
 
         saveChangesButton.addActionListener(e -> {
             try {
-                Files.write(Paths.get(filePath), textField1.getText().getBytes());
+                Files.write(Paths.get(filePath), txtArea.getText().getBytes());
                 JOptionPane.showMessageDialog(frame, "Changes saved successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
             } catch (IOException ex) {
                 System.out.println("Error saving file" + ex.getMessage());
