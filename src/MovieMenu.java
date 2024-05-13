d5dd4288f4f724fd75be3d23d2d14567b982be3e
dustinquintanilla <portillo0121@gmail.com>
2024-05-12 23:54:23 -0400

    created variables that can accept the parameters

--- src/MovieMenu.java
+++ src/MovieMenu.java
@@ -15,6 +15,10 @@ public class MovieMenu {
 
 
     public MovieMenu(boolean isGuest, String username) {
+
+        boolean isGuest1 = isGuest;
+        String username1 = username;
+
         JFrame frame = new JFrame("Movie Menu");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(700, 600);
@@ -23,57 +27,44 @@ public class MovieMenu {
         frame.setLocationRelativeTo(null);
 
 
-        // Load the image
         ImageIcon movieIcon1 = new ImageIcon(getClass().getResource("/images/titanic.jpg"));
         Image image1 = movieIcon1.getImage();
         Image scaledImage1 = image1.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
         ImageIcon scaledIcon1 = new ImageIcon(scaledImage1);
         label1.setIcon(scaledIcon1);
 
-        // Load the image
         ImageIcon movieIcon2 = new ImageIcon(getClass().getResource("/images/mario_movie.jpeg"));
-        // Scale the image
         Image image2 = movieIcon2.getImage();
         Image scaledImage2 = image2.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
-        // Create a new ImageIcon from the scaled image
         ImageIcon scaledIcon2 = new ImageIcon(scaledImage2);
-        // Set the icon of button1 to the scaled image
         label2.setIcon(scaledIcon2);
 
-        // Load the image
         ImageIcon movieIcon3 = new ImageIcon(getClass().getResource("/images/kung_fu_panda.jpeg"));
-        // Scale the image
         Image image3 = movieIcon3.getImage();
         Image scaledImage3 = image3.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
-        // Create a new ImageIcon from the scaled image
         ImageIcon scaledIcon3 = new ImageIcon(scaledImage3);
-        // Set the icon of button1 to the scaled image
         label3.setIcon(scaledIcon3);
 
-        // Load the image
         ImageIcon movieIcon4 = new ImageIcon(getClass().getResource("/images/cinderella.jpg"));
-        // Scale the image
         Image image4 = movieIcon4.getImage();
         Image scaledImage4 = image4.getScaledInstance(150, 200, Image.SCALE_SMOOTH);
-        // Create a new ImageIcon from the scaled image
         ImageIcon scaledIcon4 = new ImageIcon(scaledImage4);
-        // Set the icon of button1 to the scaled image
         label4.setIcon(scaledIcon4);
 
         button1.addActionListener(e -> {
-            new movie1_info(true, "");
+            new movie1_info(isGuest1, username1);
         });
 
         button2.addActionListener(e -> {
-            //new ticketPricing();
+            new movie2_info(isGuest1, username1);
         });
 
         button3.addActionListener(e -> {
-            //new ticketPricing();
+            new movie3_info(isGuest1, username1);
         });
 
         button4.addActionListener(e -> {
-            //new ticketPricing();
+            new movie4_info(isGuest1, username1);
         });
 
     }
